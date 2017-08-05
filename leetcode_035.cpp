4d5dcb57f52a8857e992ee8909b1a6fb07f13d90
camelshang <luoshang36@gmail.com>
2017-08-05 20:20:47 +0800

    search insert position

---
+++ leetcode_035.cpp
@@ -0,0 +1,23 @@
+/*
+Given a sorted array and a target value, return the index if the target is
+found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+*/
+int searchInsert(vector<int> &nums, int target) {
+  int start = 0, end = nums.size(), mid;
+  while (start < end) {
+    mid = start + (end - start) / 2;
+    if (nums[mid] >= target)
+      end = mid;
+    else
+      start = mid + 1;
+  }
+  return start;
+}
